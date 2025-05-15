281f4de5eb82d747ee9a273f244f4a5a03463536
Harshit Mishra <harshitmishra@Harshits-MacBook-Air.local>
2025-05-16 00:48:50 +0530

    done todays challenege

---
+++ 2025-05-15-Daily.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        
+        int n = groups.size();
+        vector<string> res;
+        for(int i=0;i<n;i++)
+        {
+            if(i==0 || groups[i] != groups[i-1])
+            {
+                res.push_back(words[i]);
+            }
+        }
+        return res;
+    }
+};
