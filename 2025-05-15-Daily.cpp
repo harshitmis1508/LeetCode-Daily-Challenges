c4f89466b1f0f1a2cb732444715ac68631d5636b
Harshit Mishra <harshitmishra@Harshits-MacBook-Air.local>
2025-05-17 14:31:36 +0530

    Solved today's problem

--- 2025-05-15-Daily.cpp
+++ 2025-05-15-Daily.cpp
@@ -1,16 +1,16 @@
-class Solution {
-public:
-    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+// class Solution {
+// public:
+//     vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
         
-        int n = groups.size();
-        vector<string> res;
-        for(int i=0;i<n;i++)
-        {
-            if(i==0 || groups[i] != groups[i-1])
-            {
-                res.push_back(words[i]);
-            }
-        }
-        return res;
-    }
-};
+//         int n = groups.size();
+//         vector<string> res;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(i==0 || groups[i] != groups[i-1])
+//             {
+//                 res.push_back(words[i]);
+//             }
+//         }
+//         return res;
+//     }
+// };
